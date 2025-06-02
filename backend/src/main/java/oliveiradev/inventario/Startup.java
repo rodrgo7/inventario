54843ad9fa6cb087c1e733e6db9d66be06f27144
Rodrgo <r.oliveira.95@hotmail.com>
2025-06-02 15:23:04 -0300

    ajustes

--- backend/src/main/java/oliveiradev/inventario/Startup.java
+++ backend/src/main/java/oliveiradev/inventario/Startup.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing // Habilita a auditoria do MongoDB)
+@EnableMongoAuditing
 public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
